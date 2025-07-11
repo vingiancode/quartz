146a12e044174a220bb988dc6862565582e480f5
vingiancode <vingian.mail@gmail.com>
2025-07-11 18:36:36 -0300

    MODE - baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://vingiancode.github.io/quartz/",
+    baseUrl: "/quence/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
